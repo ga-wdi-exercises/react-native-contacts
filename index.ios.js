e5b041c9ced96dac9042189ae709199e297ad2d1
Nick Olds <nolds9@yahoo.com>
2016-11-28 01:41:30 -0500

    adds ContactSearch to filter contacts

--- index.ios.js
+++ index.ios.js
@@ -5,11 +5,23 @@ import {
   Text,
   View,
   Image,
-  ListView
+  ListView,
+  TextInput
 } from 'react-native';
 
 // import seed data
 import contactsData from './contactsData'
+
+// ContactSearch component
+const ContactSearch = props => (
+  <View style={styles.searchContainer}>
+    <TextInput
+      style={styles.input}
+      placeholder="Search..."
+      value={props.searchTerm}
+      onChangeText={text => props.onSearchInput(text) } />
+  </View>
+)
 
 // Row component
 const Row = props => {
@@ -31,7 +43,12 @@ class ContactList extends Component {
         dataSource={this.props.dataSource}
         renderRow={data => <Row contact={data} />}
         renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-       />
+        renderHeader={() => (
+          <ContactSearch
+            searchTerm={this.props.searchTerm}
+            onSearchInput={this.props.onSearchInput} />
+        )}
+      />
     )
   }
 }
@@ -46,13 +63,35 @@ export default class ContactApp extends Component {
       dataSource: ds.cloneWithRows(contactsData),
       searchTerm: '',
     }
+
+    // bind methods to instance
+    this.handleSearchInput = this.handleSearchInput.bind(this)
+  }
+
+  handleSearchInput(searchTerm) {
+    let filterbyFullName = contact => {
+      let fullName = `${contact.firstName} ${contact.lastName}`
+      return fullName.includes(searchTerm.toLowerCase())
+    }
+
+    let updatedContacts = this.state.contacts.slice().filter(filterbyFullName)
+    updatedContacts = updatedContacts.length ? updatedContacts : this.state.contacts.slice()
+
+    this.setState({
+      ...this.state,
+      dataSource: this.state.dataSource.cloneWithRows(updatedContacts),
+      searchTerm,
+    })
   }
 
   render() {
     return (
-      <View>
+      <View style={{top: 18}}>
         <ContactList
-          dataSource={this.state.dataSource} />
+          dataSource={this.state.dataSource}
+          onSearchInput={this.handleSearchInput}
+          searchTerm={this.state.searchTerm}
+        />
       </View>
     )
   }
@@ -79,7 +118,22 @@ const styles = StyleSheet.create({
     flex: 1,
     height: StyleSheet.hairlineWidth,
     backgroundColor: '#8E8E8E',
-  }
+  },
+  searchContainer: {
+   flex: 1,
+   padding: 8,
+   flexDirection: 'row',
+   alignItems: 'center',
+   backgroundColor: '#C1C1C1',
+ },
+ input: {
+   height: 30,
+   flex: 1,
+   paddingHorizontal: 8,
+   fontSize: 15,
+   backgroundColor: '#FFFFFF',
+   borderRadius: 2,
+ },
 })
 
 AppRegistry.registerComponent('ContactApp', () => ContactApp);
